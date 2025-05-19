06cecc792426c44d88f89d61abaca7bf5ffe9f59
kan dhillon <kdhillon469@gmail.com>
2025-05-19 15:21:09 +1000

    add landing component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./components/Landing";
 
 function App() {
   return (
     <Router>
       <Nav />
+      <Landing />
       <Routes></Routes>
     </Router>
   );
