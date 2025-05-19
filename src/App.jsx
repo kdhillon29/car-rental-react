4242438438dda1142decc4ba51b61cce30883ff9
kan dhillon <kdhillon469@gmail.com>
2025-05-19 17:58:52 +1000

    add choose component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import Nav from "./components/Nav";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./components/Landing";
 import Features from "./components/Features";
-
+import Banner from "./components/Banner";
+import Choose from "./components/Choose";
 function App() {
   return (
     <Router>
       <Nav />
       <Landing />
       <Features />
+      <Banner />
+      <Choose />
       <Routes></Routes>
     </Router>
   );
