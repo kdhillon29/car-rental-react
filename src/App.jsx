e3d811f5e3673763922894cff7f6f7be60384bcc
kan dhillon <kdhillon469@gmail.com>
2025-05-20 11:12:35 +1000

    add download component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Landing from "./components/Landing";
 import Features from "./components/Features";
 import Banner from "./components/Banner";
 import Choose from "./components/Choose";
+import Testimonials from "./components/Testimonials";
+import Download from "./components/Download";
 function App() {
   return (
     <Router>
@@ -12,6 +14,8 @@ function App() {
       <Features />
       <Banner />
       <Choose />
+      <Testimonials />
+      <Download />
       <Routes></Routes>
     </Router>
   );
