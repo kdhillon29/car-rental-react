9636964a16316dd937d8a1b83532144bd1345864
kan dhillon <kdhillon469@gmail.com>
2025-05-22 13:52:52 +1000

    add dropoff date feature

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -6,7 +6,7 @@ import {
   faTimes,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const Booking = ({
   carModels,
@@ -17,6 +17,10 @@ const Booking = ({
   setSuccessOpen,
 }) => {
   const [bookingLoading, setBookingLoading] = useState(false);
+  const [pickupDate, setPickupDate] = useState(new Date());
+  const [dropDate, setDropDate] = useState(new Date());
+
+  const dropRef = useRef(null);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -116,7 +120,24 @@ const Booking = ({
                 Pick-up<span className="required">*</span>
               </span>
             </label>
-            <input type="date" className="form__item__input" required />
+            <input
+              type="date"
+              min={new Date().toISOString().split("T")[0]}
+              value={new Date(pickupDate).toISOString().split("T")[0]}
+              onChange={(event) => {
+                setPickupDate(event.target.value);
+                setDropDate(
+                  new Date(event.target.value).setDate(
+                    new Date(event.target.value).getDate() + 1
+                  )
+                );
+                dropRef.current.min = event.target.value;
+                dropRef.current.focus();
+                dropRef.current.style.borderColor = "var(--primary)";
+              }}
+              className="form__item__input"
+              required
+            />
           </div>
           <div className="form__item">
             <label className="form__item__label">
@@ -125,7 +146,15 @@ const Booking = ({
                 Drop-off<span className="required">*</span>
               </span>
             </label>
-            <input type="date" className="form__item__input" required />
+            <input
+              type="date"
+              ref={dropRef}
+              onChange={(event) => setDropDate(event.target.value)}
+              min={new Date(pickupDate).toISOString().split("T")[0]}
+              value={new Date(dropDate).toISOString().split("T")[0]}
+              className="form__item__input"
+              required
+            />
           </div>
           <div className="form__item">
             <button className="form__item__submit">
